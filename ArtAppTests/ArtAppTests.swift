c15e9179beceffdd35b7395af61bb936632ec4c7
Suharik001 <suharev-a@list.ru>
2024-12-12 13:30:15 +0300

    Initial Commit

---
+++ ArtAppTests/ArtAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAppTests.swift
+//  ArtAppTests
+//
+//  Created by Сергей Сухарев on 12.12.2024.
+//
+
+import Testing
+@testable import ArtApp
+
+struct ArtAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
